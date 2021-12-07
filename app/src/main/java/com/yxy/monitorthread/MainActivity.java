450b77666d93ace13ebd6503b89b382ff1f4b9da
yunxiangyong <yunxiangyong@zuoyebang.com>
2021-12-07 11:55:15 +0800

    8

--- app/src/main/java/com/yxy/monitorthread/MainActivity.java
+++ app/src/main/java/com/yxy/monitorthread/MainActivity.java
@@ -3,12 +3,46 @@ package com.yxy.monitorthread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.yxy.monitormodel.ThreadInfoManager;
+import com.yxy.monitormodel.bean.ThreadInfo;
+import com.yxy.monitormodel.proxy.ProxyThread;
 
 public class MainActivity extends AppCompatActivity {
+    Thread thread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.sss).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                thread=new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.i("tag5","Thread");
+                    }
+                });
+                thread.setName("yxy-thread");
+                thread.start();
+
+            }
+        });
+
+        findViewById(R.id.sss2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(thread instanceof ProxyThread){
+                    Log.i("tag5","thread instanceof ProxyThread  替换成功");
+                }
+                ThreadInfo threadInfo=ThreadInfoManager.getINSTANCE().getThreadInfoById(thread.getId());
+                if(threadInfo!=null){
+                    Log.i("tag5","threadInfo "+threadInfo.getName());
+                }
+            }
+        });
     }
 }
