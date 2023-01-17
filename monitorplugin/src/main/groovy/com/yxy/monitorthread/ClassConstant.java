139ac6a554509bb9f6c8fd99cedcc8ee2d747a66
yunxiangyong <yunxiangyong@zuoyebang.com>
2023-01-17 17:18:48 +0800

    修改版本

--- monitorplugin/src/main/groovy/com/yxy/monitorthread/ClassConstant.java
+++
@@ -1,7 +0,0 @@
-package com.yxy.monitorthread;
-
-public class ClassConstant {
-
-    static final String S_Thread = "java/lang/Thread";
-    static final String S_ProxyThread = "com/yxy/monitormodel/proxy/ProxyThread";
-}
