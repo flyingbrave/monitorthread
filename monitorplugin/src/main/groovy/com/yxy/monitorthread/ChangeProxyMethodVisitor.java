1d8938f2c4ce5235b4b64df5e113e631c90c75ce
yunxiangyong <yunxiangyong@zuoyebang.com>
2023-01-17 17:33:19 +0800

    修改版本

---
+++ monitorplugin/src/main/groovy/com/yxy/monitorthread/ChangeProxyMethodVisitor.java
@@ -0,0 +1,39 @@
+package com.yxy.monitorthread;
+
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+import static com.yxy.monitorthread.ClassConstant.S_ProxyThread;
+import static com.yxy.monitorthread.ClassConstant.S_Thread;
+
+
+public class ChangeProxyMethodVisitor extends MethodVisitor {
+    private String className;
+
+    ChangeProxyMethodVisitor(int api, MethodVisitor methodVisitor, String className) {
+        super(api, methodVisitor);
+        this.className = className;
+    }
+
+    @Override
+    public void visitTypeInsn(int opcode, String type) {
+        if (opcode == Opcodes.NEW) {
+            switch (type) {
+                case S_Thread:
+                    mv.visitTypeInsn(Opcodes.NEW, S_ProxyThread);
+                    return;
+            }
+        }
+        super.visitTypeInsn(opcode, type);
+    }
+
+    @Override
+    public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
+        if (owner.equals(S_Thread) && name.equalsIgnoreCase("<init>")) {
+            mv.visitMethodInsn(opcode, S_ProxyThread, name, descriptor, false);
+            return;
+        }
+        super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
+    }
+}
---
+++ monitorplugin/src/main/groovy/com/yxy/monitorthread/ChangeSuperMethodVisitor.java
@@ -0,0 +1,35 @@
+package com.yxy.monitorthread;
+
+
+import static com.yxy.monitorthread.ClassConstant.S_ProxyThread;
+import static com.yxy.monitorthread.ClassConstant.S_Thread;
+
+import com.android.ddmlib.Log;
+
+import org.objectweb.asm.MethodVisitor;
+
+
+public class ChangeSuperMethodVisitor extends MethodVisitor {
+    private String className;
+
+    ChangeSuperMethodVisitor(int api, MethodVisitor methodVisitor, String className) {
+        super(api, methodVisitor);
+        this.className = className;
+        Log.i("tag5","ChangeSuperMethodVisitor");
+        System.out.println("ChangeSuperMethodVisitor");
+    }
+
+    @Override
+    public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
+        if (name.equalsIgnoreCase("<init>")) {
+            switch (owner) {
+                case S_Thread:
+                    Log.i("tag5","visitMethodInsn 改继承");
+                    System.out.println("visitMethodInsn 改继承");
+                    mv.visitMethodInsn(opcode, S_ProxyThread, name, descriptor, false);
+                    return;
+            }
+        }
+        super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
+    }
+}
