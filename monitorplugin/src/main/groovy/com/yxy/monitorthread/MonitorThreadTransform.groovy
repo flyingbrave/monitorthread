b82a2cd0b890515f9a724c25d6189e72de1bfb2a
yunxiangyong <yunxiangyong@zuoyebang.com>
2023-01-17 18:19:26 +0800

    修改版本

--- monitorplugin/src/main/groovy/com/yxy/monitorthread/MonitorThreadTransform.groovy
+++ monitorplugin/src/main/groovy/com/yxy/monitorthread/MonitorThreadTransform.groovy
@@ -58,108 +58,107 @@ class MonitorThreadTransform extends Transform implements Plugin<Project> {
         if (outputProvider != null)
             outputProvider.deleteAll()
         System.out.println("transform threads...")
-//        JarInput threadtrackerJarInput = null
         inputs.each { TransformInput input ->
 
             input.directoryInputs.each { DirectoryInput directoryInput ->
                 System.out.println("handleDirectoryInput")
-                handleDirectoryInput(directoryInput, outputProvider)
+//                handleDirectoryInput(directoryInput, outputProvider)
             }
 
             input.jarInputs.each { JarInput jarInput ->
                 System.out.println("handleJarInputs")
-                handleJarInputs(jarInput, outputProvider)
+//                handleJarInputs(jarInput, outputProvider)
             }
             def cost = (System.currentTimeMillis() - startTime) / 1000
             System.out.println("ThreadTrackerTransform cost ： $cost s")
         }
     }
-    static void handleDirectoryInput(DirectoryInput directoryInput, TransformOutputProvider outputProvider) {
-        if (directoryInput.file.isDirectory()) {
-            // 遍历目录所有文件（包含子文件）
-            directoryInput.file.eachFileRecurse { File file ->
-                def name = file.name
-                if (checkClassFile(name, false)) {
-                    // println '----------- class <' + name + '> -----------'
-                    ClassReader classReader = new ClassReader(file.bytes)
-                    ClassWriter classWriter = new ClassWriter(classReader, ClassWriter.COMPUTE_MAXS)
-                    ClassVisitor cv = new MonitorThreadClassVisitor(classWriter, null)
-                    classReader.accept(cv, EXPAND_FRAMES)
-                    byte[] code = classWriter.toByteArray()
-                    FileOutputStream fos = new FileOutputStream(
-                            file.parentFile.absolutePath + File.separator + name)
-                    fos.write(code)
-                    fos.close()
-                }
-            }
-        }
+//    static void handleDirectoryInput(DirectoryInput directoryInput, TransformOutputProvider outputProvider) {
+//        if (directoryInput.file.isDirectory()) {
+//            // 遍历目录所有文件（包含子文件）
+//            directoryInput.file.eachFileRecurse { File file ->
+//                def name = file.name
+//                if (checkClassFile(name, false)) {
+//                    // println '----------- class <' + name + '> -----------'
+//                    ClassReader classReader = new ClassReader(file.bytes)
+//                    ClassWriter classWriter = new ClassWriter(classReader, ClassWriter.COMPUTE_MAXS)
+//                    ClassVisitor cv = new MonitorThreadClassVisitor(classWriter, null)
+//                    classReader.accept(cv, EXPAND_FRAMES)
+//                    byte[] code = classWriter.toByteArray()
+//                    FileOutputStream fos = new FileOutputStream(
+//                            file.parentFile.absolutePath + File.separator + name)
+//                    fos.write(code)
+//                    fos.close()
+//                }
+//            }
+//        }
+//
+//        // 固定写法 把输出给下一个任务
+//        def dest = outputProvider.getContentLocation(directoryInput.name,
+//                directoryInput.contentTypes, directoryInput.scopes,
+//                Format.DIRECTORY)
+//        FileUtils.copyDirectory(directoryInput.file, dest)
+//    }
 
-        // 固定写法 把输出给下一个任务
-        def dest = outputProvider.getContentLocation(directoryInput.name,
-                directoryInput.contentTypes, directoryInput.scopes,
-                Format.DIRECTORY)
-        FileUtils.copyDirectory(directoryInput.file, dest)
-    }
-
-    static void handleJarInputs(JarInput jarInput, TransformOutputProvider outputProvider) {
-        if (jarInput.file.getAbsolutePath().endsWith(".jar")) {
-            def jarName = jarInput.name
-            // println '----------- jarName <' + jarName + '> -----------'
-            def md5Name = DigestUtils.md5Hex(jarInput.file.getAbsolutePath())
-            if (jarName.endsWith(".jar")) {
-                jarName = jarName.substring(0, jarName.length() - 4)
-            }
-            JarFile jarFile = new JarFile(jarInput.file)
-            Enumeration enumeration = jarFile.entries()
-            File tmpFile = new File(jarInput.file.getParent() + File.separator + "classes_temp.jar")
-            if (tmpFile.exists()) {
-                tmpFile.delete()
-            }
-            JarOutputStream jarOutputStream = new JarOutputStream(new FileOutputStream(tmpFile))
-
-            while (enumeration.hasMoreElements()) {
-                JarEntry jarEntry = (JarEntry) enumeration.nextElement()
-                String entryName = jarEntry.getName()
-                ZipEntry zipEntry = new ZipEntry(entryName)
-                InputStream inputStream = jarFile.getInputStream(jarEntry)
-                // println '----------- jarClass <' + entryName + '> -----------'
-                if (checkClassFile(entryName, true)) {
-                    jarOutputStream.putNextEntry(zipEntry)
-                    ClassReader classReader = new ClassReader(IOUtils.toByteArray(inputStream))
-                    ClassWriter classWriter = new ClassWriter(classReader, ClassWriter.COMPUTE_MAXS)
-                    ClassVisitor cv = new MonitorThreadClassVisitor(classWriter, jarName)
-                    classReader.accept(cv, EXPAND_FRAMES)
-                    byte[] code = classWriter.toByteArray()
-                    jarOutputStream.write(code)
-                } else {
-                    jarOutputStream.putNextEntry(zipEntry)
-                    jarOutputStream.write(IOUtils.toByteArray(inputStream))
-                }
-                jarOutputStream.closeEntry()
-            }
-            jarOutputStream.close()
-            jarFile.close()
-            def dest = outputProvider.getContentLocation(jarName + md5Name,
-                    jarInput.contentTypes, jarInput.scopes, Format.JAR)
-            FileUtils.copyFile(tmpFile, dest)
-            tmpFile.delete()
-        }
-    }
+//    static void handleJarInputs(JarInput jarInput, TransformOutputProvider outputProvider) {
+//        if (jarInput.file.getAbsolutePath().endsWith(".jar")) {
+//            def jarName = jarInput.name
+//            // println '----------- jarName <' + jarName + '> -----------'
+//            def md5Name = DigestUtils.md5Hex(jarInput.file.getAbsolutePath())
+//            if (jarName.endsWith(".jar")) {
+//                jarName = jarName.substring(0, jarName.length() - 4)
+//            }
+//            JarFile jarFile = new JarFile(jarInput.file)
+//            Enumeration enumeration = jarFile.entries()
+//            File tmpFile = new File(jarInput.file.getParent() + File.separator + "classes_temp.jar")
+//            if (tmpFile.exists()) {
+//                tmpFile.delete()
+//            }
+//            JarOutputStream jarOutputStream = new JarOutputStream(new FileOutputStream(tmpFile))
+//
+//            while (enumeration.hasMoreElements()) {
+//                JarEntry jarEntry = (JarEntry) enumeration.nextElement()
+//                String entryName = jarEntry.getName()
+//                ZipEntry zipEntry = new ZipEntry(entryName)
+//                InputStream inputStream = jarFile.getInputStream(jarEntry)
+//                // println '----------- jarClass <' + entryName + '> -----------'
+//                if (checkClassFile(entryName, true)) {
+//                    jarOutputStream.putNextEntry(zipEntry)
+//                    ClassReader classReader = new ClassReader(IOUtils.toByteArray(inputStream))
+//                    ClassWriter classWriter = new ClassWriter(classReader, ClassWriter.COMPUTE_MAXS)
+//                    ClassVisitor cv = new MonitorThreadClassVisitor(classWriter, jarName)
+//                    classReader.accept(cv, EXPAND_FRAMES)
+//                    byte[] code = classWriter.toByteArray()
+//                    jarOutputStream.write(code)
+//                } else {
+//                    jarOutputStream.putNextEntry(zipEntry)
+//                    jarOutputStream.write(IOUtils.toByteArray(inputStream))
+//                }
+//                jarOutputStream.closeEntry()
+//            }
+//            jarOutputStream.close()
+//            jarFile.close()
+//            def dest = outputProvider.getContentLocation(jarName + md5Name,
+//                    jarInput.contentTypes, jarInput.scopes, Format.JAR)
+//            FileUtils.copyFile(tmpFile, dest)
+//            tmpFile.delete()
+//        }
+//    }
 
 
     /**
      * 过滤class文件名
      */
-    static boolean checkClassFile(String name, boolean isJar) {
-        if (isJar) {
-            int lastIndex = name.lastIndexOf('/')
-            if (lastIndex != -1) {
-                name = name.substring(lastIndex + 1, name.length())
-            }
-            // 只要/后面的文件名，以便后续调用startsWith判断
-        }
-        return (name.endsWith(".class") && !name.startsWith("R\$")
-                && name != "R.class" && !name.startsWith("BR\$")
-                && name != "BR.class" && name != "BuildConfig.class")
-    }
+//    static boolean checkClassFile(String name, boolean isJar) {
+//        if (isJar) {
+//            int lastIndex = name.lastIndexOf('/')
+//            if (lastIndex != -1) {
+//                name = name.substring(lastIndex + 1, name.length())
+//            }
+//            // 只要/后面的文件名，以便后续调用startsWith判断
+//        }
+//        return (name.endsWith(".class") && !name.startsWith("R\$")
+//                && name != "R.class" && !name.startsWith("BR\$")
+//                && name != "BR.class" && name != "BuildConfig.class")
+//    }
 }
